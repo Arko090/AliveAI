de64d73ba3310cfe064395c60924d7e648e7d594
Arko Sengupta <arkosengupta9@gmail.com>
2024-06-25 08:56:08 +0530

    MenuDashboard

--- src/Components/MenuDashboard.js
+++ src/Components/MenuDashboard.js
@@ -1,4 +1,19 @@
+import { Container, Row, Col, Button, Image } from "react-bootstrap";
+import "../StyleSheets/Comp.css";
 
-const MenuDashboard = () => <h1>Dashboard Component</h1>;
+const MenuDashboard = () => {
+    return (
+        <Container>
+            <Row>
+                <Col className="col-6">
+                {/* First Half*/}
+                </Col>
+                <Col>
+                {/* Second Half */}
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default MenuDashboard;
