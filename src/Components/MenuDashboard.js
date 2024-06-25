a6cd53c6893ee616bd2864768d70a07cc99bf3a7
Rajita Roy <68264150+rajitaroy@users.noreply.github.com>
2024-06-25 20:53:40 +0530

    Donut chart

--- src/Components/MenuDashboard.js
+++ src/Components/MenuDashboard.js
@@ -1,16 +1,38 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import DonutChart from "./DonutChart";
 import "../StyleSheets/Comp.css";
 
 const MenuDashboard = () => {
+    const dataSets = [
+        { id: '1', risk: 25, health: 75 },
+        { id: '2', risk: 25, health: 75 },
+        { id: '3', risk: 25, health: 75 },
+        { id: '4', risk: 25, health: 75 },
+        { id: '5', risk: 25, health: 75 },
+        { id: '6', risk: 25, health: 75 },
+        { id: '7', risk: 25, health: 75 },
+        { id: '8', risk: 25, health: 75 },
+    ];
+
     return (
         <Container>
             <Row>
                 <Col className="col-6">
-                <h1>First Half of Dashboard</h1>
+                    <h1>First Half of Dashboard</h1>
                 </Col>
-                <Col>
-                <h1>Second Half of Dashboard</h1>
+                <Col style={{ height: '880px', overflowY: 'auto' }}>
+                    <h2>Second Half of Dashboard</h2>
+                    <Row>
+                        {dataSets.map(data => (
+                            <Col key={data.id} md={6}>
+                                <DonutChart data={[
+                                    { label: 'Risk', value: data.risk }, 
+                                    { label: 'Health', value: data.health }
+                                ]} />
+                            </Col>
+                        ))}
+                    </Row>
                 </Col>
             </Row>
         </Container>
